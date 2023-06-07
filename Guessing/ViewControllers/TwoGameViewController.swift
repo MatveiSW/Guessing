6c98e3a38ef51fd9edc3ec8942357b39a1816340
Deniss Akisins <Denjaa3@icloud.com>
2023-06-07 20:31:23 +0300

    Last update

--- Guessing/ViewControllers/TwoGameViewController.swift
+++ Guessing/ViewControllers/TwoGameViewController.swift
@@ -12,18 +12,22 @@ class TwoGameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        settingBackroundImage()
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+private extension TwoGameViewController {
+    func settingBackroundImage() {
+        let backroundImage = UIImageView(image: UIImage(named: "GameVCBackground"))
+        backroundImage.frame = view.bounds
+        backroundImage.contentMode = .scaleAspectFill
+        backroundImage.alpha = 0.3
+        
+        view.addSubview(backroundImage)
+        view.sendSubviewToBack(backroundImage)
     }
-    */
-
 }
--- Guessing/ViewControllers/TwoTabBarCotroller.swift
+++ Guessing/ViewControllers/TwoTabBarCotroller.swift
@@ -8,22 +8,35 @@
 import UIKit
 
 class TwoTabBarCotroller: UITabBarController {
-
+    
+    
+    var game: Game!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let viewControllers = viewControllers else { return }
+        for viewController in viewControllers {
+            if let twoRulesVC = viewController as? TwoRulesViewController {
+                twoRulesVC.game = game
+            } else {
+                guard let twoGameVC = viewController as? TwoRulesViewController else { return}
+                twoGameVC.game = game
+                
+            }
+        }
+        
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
