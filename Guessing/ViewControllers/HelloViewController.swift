ac2c37141b37fadc65008632e6cb49b2e9361353
Zalman Zoteev <sz.zoteev@gmail.com>
2023-06-08 20:47:11 +0300

    update helloScreen

--- Guessing/ViewControllers/HelloViewController.swift
+++ Guessing/ViewControllers/HelloViewController.swift
@@ -8,24 +8,37 @@
 import UIKit
 
 class HelloViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        settingButtonStart()
         settingBackround()
-        
     }
    
-
+    
 }
-
-extension HelloViewController {
-    func settingBackround() {
-        let backroundImage = UIImageView(image: UIImage(named: "Hello"))
-        backroundImage.frame = view.bounds
-        backroundImage.contentMode = .scaleAspectFill
+    
+    extension HelloViewController {
         
-        view.addSubview(backroundImage)
-        view.sendSubviewToBack(backroundImage)
+        func settingButtonStart() {
+            
+            let button = UIButton(type: .custom)
+            button.frame = CGRect(x: 120, y: 530, width: 150, height: 100)
+            let image = UIImage(named: "startButtonpng")
+            button.setBackgroundImage(image, for: .normal)
+            view.addSubview(button)
+            button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        }
+        @objc func buttonTapped() {
+            performSegue(withIdentifier: "toGameList", sender: self)
+        }
+        
+        func settingBackround() {
+            let backroundImage = UIImageView(image: UIImage(named: "backGroundHello"))
+            backroundImage.frame = view.bounds
+            backroundImage.contentMode = .scaleAspectFill
+            
+            view.addSubview(backroundImage)
+            view.sendSubviewToBack(backroundImage)
+        }
     }
-}
